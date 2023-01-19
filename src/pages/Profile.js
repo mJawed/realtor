6aae6dee7015c8f1c13b01f9c2c18dc91780cebd
Mohd Jawed <mohd.jawed@alcanttor.com>
2023-01-19 23:54:10 +0530

    Profile Edit

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,12 @@
 
 
-import { getAuth } from "firebase/auth"
+import { getAuth, updateProfile } from "firebase/auth"
+import { doc, updateDoc } from "firebase/firestore"
 import{useState}from"react"
 import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
+import { db } from "../firebase"
+
 
 const auth = getAuth()
 
@@ -18,10 +22,13 @@ if(username===null){
   username = "fill your username"
 }
 
-const[userName,setuserName]= useState(
-  username)
+
+const[userName,setuserName]= useState(username)
 
 const[userEmail,setuserEmail]= useState(auth.currentUser.email)
+
+const[updateData,setupdateData]=useState(false)
+
 
 
 function handleLogout (){
@@ -30,10 +37,37 @@ navigate('/')
 }
 
 
-function handleSubmit(e){
-  e.preventDefault()
-}
+async function handleSubmit(e){
 
+  console.log(userName,userEmail)
+
+  console.log('Hey man you hit me')
+  console.log(auth.currentUser.displayName);
+
+  try {
+
+   if( auth.currentUser.displayName !== userName){
+
+await updateProfile(auth.currentUser,{
+  displayName:userName,
+});
+
+const docRef = doc(db, 'users', auth.currentUser.uid)
+await updateDoc(docRef,{
+  userName:userName
+})
+
+toast.success('Profile Successfully Updatded')
+
+   }
+    
+  } catch (error) {
+
+    toast.error("ss")
+    
+  }
+ 
+}
 
     return ( 
 
@@ -41,13 +75,13 @@ function handleSubmit(e){
             <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
         <div className="w-full md:w-[50%] mt-6 px-3">
-          <form method="post" onSubmit={(e)=>handleSubmit(e)} >
+          <form method="post" onSubmit={handleSubmit} >
             {/* Name Input */}
 
             <input
               type="text"
               id="name"
-              disabled
+               disabled={!updateData}
               value={userName}
               onChange={(e)=>{setuserName(e.target.value)}}
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
@@ -59,7 +93,7 @@ function handleSubmit(e){
             <input
               type="email"
               id="email"
-              disabled
+             disabled={!updateData}
             value={userEmail}
             onChange={(e)=>{setuserEmail(e.target.value)}}
               
@@ -70,10 +104,15 @@ function handleSubmit(e){
               <p className="flex items-center ">
                 Do you want to change your name?
                 <span
-                
+                 onClick={()=>{
+                  updateData && handleSubmit();
+                  setupdateData((prevState)=>!prevState)}}
                   className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
                 >
-                 
+                  {updateData ? "Apply changes" : "Edit"}
+                  
+
+               
                 </span>
               </p>
               <p 
@@ -88,7 +127,7 @@ function handleSubmit(e){
             type="submit"
             className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
           >
-            
+            Submit
           </button>
         </div>
       </section>
